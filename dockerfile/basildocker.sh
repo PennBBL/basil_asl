6548574cae82383f4ce159927bb612cf26827173
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2018-11-26 13:49:10 -0500

    Update basildocker.sh

--- dockerfile/basildocker.sh
+++ dockerfile/basildocker.sh
@@ -5,14 +5,14 @@ neurodocker \
   --install wget  \
   --fsl version=5.0.11 \
   --ants version=2.2.0 \
-  --env BASILDIR=/basil_asl-version2  FSLDIR=/opt/fsl-5.0.11 \
-  --add-to-entrypoint 'export BASILDIR=/basil_asl-version2' \
+  --env BASILDIR=/basil_asl-master  FSLDIR=/opt/fsl-5.0.11 \
+  --add-to-entrypoint 'export BASILDIR=/basil_asl-master' \
   --add-to-entrypoint 'export FSLDIR=/opt/fsl-5.0.11' \
   --add-to-entrypoint 'export ANTSPATH=/opt/ants-2.2.0' \
   --add-to-entrypoint 'export PATH=$PATH:$BASILDIR' \
-  --run-bash 'cd / && wget https://github.com/PennBBL/basil_asl/archive/version2.zip && unzip version2.zip && chmod +x /basil_asl-version2/*' \
-  --run-bash 'BASILDIR=/basil_asl-version2 FSLDIR=/opt/fsl-5.0.11  ANTSPATH=/opt/ants-2.2.0 ' \
+  --run-bash 'cd / && wget https://github.com/PennBBL/basil_asl/archive/master.zip && unzip master.zip && chmod +x /basil_asl-master/*' \
+  --run-bash 'BASILDIR=/basil_asl-master FSLDIR=/opt/fsl-5.0.11  ANTSPATH=/opt/ants-2.2.0 ' \
   --run-bash ' echo ==========================' \
   --run-bash 'cat $ND_ENTRYPOINT' \
   --run-bash ' echo ==========================' \
-  --entrypoint '/neurodocker/startup.sh /basil_asl-version2/perfusion_basil "$@"' > Singularity
+  --entrypoint '/neurodocker/startup.sh /basil_asl-master/perfusion_basil "$@"' > Singularity
